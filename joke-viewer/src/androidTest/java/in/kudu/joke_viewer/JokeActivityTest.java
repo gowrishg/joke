a96f2e992b9370073cba860e5defbc286d224e6d
gowrishg-mac-bookpro <gowrishg@qi.sg>
2016-06-25 19:24:05 +0800

    Updates the Instrumentation testing using espresso to validate the display text.

--- joke-viewer/src/androidTest/java/in/kudu/joke_viewer/JokeActivityTest.java
+++ joke-viewer/src/androidTest/java/in/kudu/joke_viewer/JokeActivityTest.java
@@ -1,14 +1,21 @@
 package in.kudu.joke_viewer;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.test.espresso.Espresso;
+import android.support.test.InstrumentationRegistry;
+import static android.support.test.espresso.Espresso.onView;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -25,20 +32,21 @@ public class JokeActivityTest {
             false);   // launchActivity. False to customize the intent
 
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
     public void display_joke_test() {
         Intent intent = new Intent();
         intent.putExtra(JokeActivity.JOKE_KEY, "Test Joke");
         activityRule.launchActivity(intent);
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        onView(withId(R.id.joke)).check(matches(withText("Test Joke")));
     }
+
+
+
+
 }
