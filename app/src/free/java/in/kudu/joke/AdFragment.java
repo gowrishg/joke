427ffa4192f133e33dbd49590427fdc3277e441c
gowrishg-mac-bookpro <gowrishg@qi.sg>
2016-06-25 21:13:10 +0800

    Includes AD for the Free version of the application. The ad library is only available for Free version.

---
+++ app/src/free/java/in/kudu/joke/AdFragment.java
@@ -0,0 +1,73 @@
+package in.kudu.joke;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AdFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AdFragment extends Fragment {
+
+    private AdView mAdView;
+
+    public AdFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment AdFragment.
+     */
+    public static AdFragment newInstance() {
+        AdFragment fragment = new AdFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MobileAds.initialize(getActivity(), getString(R.string.ad_app_id));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_ad, container, false);
+        mAdView = (AdView) view.findViewById(R.id.ad_view);
+
+        // Create an ad request. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("D83716E5D1E7C64AA4BE9609D5B776FA")
+                .build();
+
+        mAdView.loadAd(adRequest);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
--- app/src/main/java/in/kudu/joke/MainActivity.java
+++ app/src/main/java/in/kudu/joke/MainActivity.java
@@ -1,21 +1,29 @@
 package in.kudu.joke;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import in.kudu.joke_viewer.JokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mTellAJokeButton;
+    FrameLayout mAdFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAdFragment = (FrameLayout) findViewById(R.id.ad_fragment);
         mTellAJokeButton = (Button) findViewById(R.id.tell_a_joke);
         mTellAJokeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,10 +40,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         boolean isAdEnabled = getResources().getBoolean(R.bool.enable_advertisement);
-        if (isAdEnabled)
+        if (isAdEnabled) {
+            //! using reflection as the Class wouldn't be compilable in other flavour
+            Class c = null;
+            Method method = null;
+            try {
+                c = Class.forName("in.kudu.joke.AdFragment");
+                method = c.getMethod("newInstance");
+                Fragment adFragment = (Fragment) method.invoke(null);
+                getSupportFragmentManager().beginTransaction().add(R.id.ad_fragment, adFragment).commit();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            mAdFragment.setVisibility(View.VISIBLE);
             mTellAJokeButton.setText(R.string.free_joke);
-        else {
+        } else {
+            mAdFragment.setVisibility(View.GONE);
             mTellAJokeButton.setText(R.string.paid_joke);
         }
+
     }
 }
